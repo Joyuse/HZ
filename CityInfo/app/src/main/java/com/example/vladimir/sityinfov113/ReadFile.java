3324faca959379e588055af33f4b2219f6e3e410
Joyuse <vlt18@mail.ru>
2017-09-29 13:36:54 +0700

    фыв
    фыв

--- CityInfo/app/src/main/java/com/example/vladimir/sityinfov113/ReadFile.java
+++ CityInfo/app/src/main/java/com/example/vladimir/sityinfov113/ReadFile.java
@@ -34,10 +34,10 @@ public class ReadFile {
             //задаем размер для буфера
             byte[] buffer = new byte[fin.available()];
             //считываем и заносим в буфер
-            fin.read(buffer, 0, fin.available());
+            fin.read(buffer, 0, 4);
             ByteBuffer bb = ByteBuffer.wrap(buffer);
-            count_points = bb.getInt(); // кол-во точек
             bb.order(ByteOrder.LITTLE_ENDIAN);
+            count_points = bb.getInt(); // кол-во точек
             Log.w("W","кол-во точек = " + count_points);
             //переводим это кол-во в байты
             count_mul = count_points * 12;
@@ -45,23 +45,26 @@ public class ReadFile {
             //обьявляем массив размерностью точек
             mass_vertices = new float[count_points * 3]; //NEW было count_mul стало  count_points * 3
             //Заносим в массив
+
             int p =0;
             for(int i = 0; i < count_points * 3 ; i ++) {
                 mass_vertices[i] = bb.getFloat();
                 p+=4;
             }
+            //Проверка
+            Log.w("W","X = " + mass_vertices[0]);
+            Log.w("W","Y = " + mass_vertices[1]);
+            Log.w("W","Z = " + mass_vertices[2]);
             Log.w("W","кол-во счит байтов = " + p);
-            bb.order(ByteOrder.BIG_ENDIAN);
+
             //забираем инт
             Log.w("W","кол-во цветных точек = " + bb.getInt());
-            //забираем флоаты
-            bb.order(ByteOrder.LITTLE_ENDIAN);
+           //забираем флоаты
             Log.w("W","X = " + bb.getFloat());
             Log.w("W","Y = " + bb.getFloat());
             Log.w("W","Z = " + bb.getFloat());
             //берем цвет
-            bb.order(ByteOrder.BIG_ENDIAN);
-            Log.w("W","цвет = " + bb.getInt());
+            //Log.w("W","цвет = " + Integer.);
         }
         //Ловим ошибку при считывании
         catch (IOException ex) {
@@ -71,3 +74,37 @@ public class ReadFile {
         return mass_vertices;
     }
 }
+
+//цвет = -1261015297
+
+/*
+09-29 12:04:00.526 9624-9624/com.example.vladimir.sityinfov113 W/W: СЧИТЫВАЕМ ФАЙЛ
+09-29 12:04:00.526 9624-9624/com.example.vladimir.sityinfov113 I/System.out: Размер файла: 136507176 байт
+09-29 12:04:00.996 9624-9636/com.example.vladimir.sityinfov113 W/art: Suspending all threads took: 13.269ms
+09-29 12:04:01.426 9624-9624/com.example.vladimir.sityinfov113 W/W: кол-во точек = 793966
+09-29 12:04:01.426 9624-9624/com.example.vladimir.sityinfov113 W/W: кол-во байт которые занимают точки = 9527592
+09-29 12:04:01.946 9624-9624/com.example.vladimir.sityinfov113 W/W: X = 18230.195
+09-29 12:04:01.946 9624-9624/com.example.vladimir.sityinfov113 W/W: Y = 21232.865
+09-29 12:04:01.946 9624-9624/com.example.vladimir.sityinfov113 W/W: Z = 0.0
+09-29 12:04:01.946 9624-9624/com.example.vladimir.sityinfov113 W/W: кол-во счит байтов = 9527592
+09-29 12:04:01.946 9624-9624/com.example.vladimir.sityinfov113 W/W: кол-во цветных точек = 70255
+09-29 12:04:01.946 9624-9624/com.example.vladimir.sityinfov113 W/W: X = 38541.46
+09-29 12:04:01.946 9624-9624/com.example.vladimir.sityinfov113 W/W: Y = 10218.448
+09-29 12:04:01.946 9624-9624/com.example.vladimir.sityinfov113 W/W: Z = 0.0
+09-29 12:04:01.946 9624-9624/com.example.vladimir.sityinfov113 W/W: цвет = -9513292
+*/
+/*
+W/W: СЧИТЫВАЕМ ФАЙЛ
+09-29 12:05:53.640 13259-13259/com.example.vladimir.sityinfov113 I/System.out: Размер файла: 141387112 байт
+09-29 12:06:02.830 13259-13259/com.example.vladimir.sityinfov113 W/W: кол-во точек = 793974
+09-29 12:06:02.830 13259-13259/com.example.vladimir.sityinfov113 W/W: кол-во байт которые занимают точки = 9527688
+09-29 12:06:03.610 13259-13259/com.example.vladimir.sityinfov113 W/W: X = 18230.195
+09-29 12:06:03.610 13259-13259/com.example.vladimir.sityinfov113 W/W: Y = 21232.865
+09-29 12:06:03.610 13259-13259/com.example.vladimir.sityinfov113 W/W: Z = 0.0
+09-29 12:06:03.610 13259-13259/com.example.vladimir.sityinfov113 W/W: кол-во счит байтов = 9527688
+09-29 12:06:03.610 13259-13259/com.example.vladimir.sityinfov113 W/W: кол-во цветных точек = 70256
+09-29 12:06:03.610 13259-13259/com.example.vladimir.sityinfov113 W/W: X = 38541.46
+09-29 12:06:03.610 13259-13259/com.example.vladimir.sityinfov113 W/W: Y = 10218.448
+09-29 12:06:03.610 13259-13259/com.example.vladimir.sityinfov113 W/W: Z = 0.0
+09-29 12:06:03.610 13259-13259/com.example.vladimir.sityinfov113 W/W: цвет = -1261015297
+ */
